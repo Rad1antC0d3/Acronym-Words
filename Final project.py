be86ef3edf8d89f380e5c18bf4b99377d78a1608
Anshul Choudhary <95236614+AnshulChoudhary001@users.noreply.github.com>
2022-11-20 00:44:04 +0530

    Add files via upload

---
+++ Final project.py
@@ -0,0 +1,30 @@
+'''You need to write a python script that generates an acronym word from a given sentence.'''
+print("So firstly start with, what are acronym words - ")
+print("An acronym is a short form of a word created by long words or phrases such as NLP for natural language processing")
+# Importing the required modules
+
+import re
+import sys
+
+
+# Defining the function to generate the acronym
+
+def acronym_generator(sentence):
+    '''This function generates an acronym from a given sentence'''
+    acronym = ''
+    for word in sentence.split():
+        acronym += word[0].upper()
+    return acronym
+
+# Defining the main function
+
+def main():
+    '''This is the main function'''
+    sentence = input('Enter a sentence: ')
+    print(acronym_generator(sentence))
+
+# Calling the main function
+
+if __name__ == '__main__':
+    main()
+    
